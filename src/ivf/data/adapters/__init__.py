282b56838c42221240761a3cd7a2fa6fb92ac70b
chinhnddev <nguyenchinh93tb@gmail.com>
2026-01-01 14:58:57 +0700

    update new

--- src/ivf/data/adapters/__init__.py
+++ src/ivf/data/adapters/__init__.py
@@ -3,7 +3,12 @@
 from .blastocyst_kaggle import BlastocystKaggleDataset, load_blastocyst_records, records_to_dataframe
 from .humanembryo2 import HumanEmbryo2Dataset, load_humanembryo2_records, records_to_dataframe as humanembryo2_records_to_dataframe
 from .hungvuong import HungVuongDataset, load_hungvuong_records, records_to_dataframe as hungvuong_records_to_dataframe
-from .synthetic import create_synthetic_splits, generate_synthetic_metadata
+
+try:
+    from .synthetic import create_synthetic_splits, generate_synthetic_metadata
+except ImportError:
+    create_synthetic_splits = None
+    generate_synthetic_metadata = None
 
 __all__ = [
     "BlastocystKaggleDataset",
@@ -15,6 +20,7 @@ __all__ = [
     "humanembryo2_records_to_dataframe",
     "load_hungvuong_records",
     "hungvuong_records_to_dataframe",
-    "create_synthetic_splits",
-    "generate_synthetic_metadata",
 ]
+
+if create_synthetic_splits and generate_synthetic_metadata:
+    __all__ += ["create_synthetic_splits", "generate_synthetic_metadata"]
